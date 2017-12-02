9d4403516126e0afa01a052bd297ff5b5a9b63fa
0x0 <ghstoreinvesting@gmail.com>
2017-12-02 19:59:22 +0800

    Add users model

---
+++ app_server/models/users.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var crypto = require('cryto');
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        require: true
+    }, 
+    hash: String,
+    salt: String
+});
+
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.valiPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
