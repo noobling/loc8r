1702b0cc6b40d7bd79e727782916fb7af133ac29
0x0 <ghstoreinvesting@gmail.com>
2017-12-02 20:57:14 +0800

    add jwt generation function

--- app_server/models/users.js
+++ app_server/models/users.js
@@ -25,3 +25,15 @@ userSchema.methods.valiPassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
     return this.hash === hash;
 };
+
+userSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7); // set expiry to 7 days from current day
+    
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiry.getTime()/1000)
+    }, 'thisissecret') // probably should use an environment variable
+}
